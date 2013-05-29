365e3d5ea0ce86fad17db4ab3f051972559c2406
Brandon-Lyons <brandon.joseph.lyons@gmail.com>
2013-05-29 11:52:41 -0500

    initial commit adding rails files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NSSBasicBlog::Application.config.secret_token = 'f02ee2a781f27e6802d4969df8f51ad130e082066579093dec2606260055326244340b29d6f54ff2c78330ee92ed6e4da5184e77f8a9eb7c04c1f79aa6cef165'
