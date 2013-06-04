55d0098a3d0c02ffa0ba7dd91d9cf2a2d6afe92f
Brandon-Lyons <brandon.joseph.lyons@gmail.com>
2013-06-04 10:24:35 -0500

    first integration test is now passing

--- test/integration/anonymous_posts_article_test.rb
+++ test/integration/anonymous_posts_article_test.rb
@@ -2,12 +2,6 @@ require 'test_helper'
 
 class AnonymousPostsArticleTest < ActionDispatch::IntegrationTest
 
-	test "count articles" do
-		Post.create(title: "First Post", body: "first")
-		Post.create(title: "Wow, its been a while", body: "but still not a writer")
-		assert_equal 2, Post.all.size
-	end
-
 	test "browse articles" do
 		Post.create(title: "First Post", body: "first")
 		Post.create(title: "Wow, its been a while", body: "but still not a writer")
@@ -15,6 +9,7 @@ class AnonymousPostsArticleTest < ActionDispatch::IntegrationTest
 		visit '/posts'
 		#assume we have a page variable
 		# assert page.has_content? "First Post"
+		assert_include page.body, "All Posts"
 		assert_include page.body, "First Post"
 		assert_include page.body, "Wow, its been a while" 
 	end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,6 +11,7 @@ end
 
 class ActionDispatch::IntegrationTest
 	include Capybara::DSL
+	self.use_transactional_fixtures = false
 end
 
 Capybara.default_driver = :selenium
